44ea7f2ec73c1df46f5e6ab05b3a66b6683f04de
minhad-mahmud2500 <87450136+minhad-mahmud2500@users.noreply.github.com>
2022-10-04 14:23:34 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,15 +11,3 @@ for post in r.subreddit("manga").new(limit=None):
 
 if selftext != "":
     r.subreddit("oneshot_daily").submit("Oneshots/First Chapters",selftext=selftext)    
-'''
-import praw
-reddit = praw.Reddit('bot1')
-subreddit =reddit.subreddit("manga")
-for submission in subreddit.new(limit=500):
-              
-        if "Oneshot" in submission.title:
-            print(submission.title)
-            print(submission.url)
-        elif "One Piece" in submission.title:
-            print(submission.title)
-            print(submission.url)
