a661cd1d1646e9d528d01f7121b2c67e09ceb5a8
minhad-mahmud2500 <87450136+minhad-mahmud2500@users.noreply.github.com>
2022-10-04 14:16:14 -0400

    Update bot.py
    finished bot

--- bot.py
+++ bot.py
@@ -1,12 +1,25 @@
 import praw
+import re
+regex = re.compile(r"(one ?shot)|(ch(apter)? ?0*1)\b", re.IGNORECASE)
+r = praw.Reddit("bot1")
+selftext = ""
+for post in r.subreddit("manga").new(limit=None):
+    if not post.title.startswith("["): continue
+    if regex.search(post.title):
+        with open("manga.txt","a") as f:
+            selftext += f"[{post.title}]({post.url})\n\n"
+
+if selftext != "":
+    r.subreddit("oneshot_daily").submit("Oneshots/First Chapters",selftext=selftext)    
+'''
+import praw
 reddit = praw.Reddit('bot1')
 subreddit =reddit.subreddit("manga")
 for submission in subreddit.new(limit=500):
-        
+              
         if "Oneshot" in submission.title:
             print(submission.title)
             print(submission.url)
         elif "One Piece" in submission.title:
             print(submission.title)
             print(submission.url)
-            
